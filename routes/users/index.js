a387dd206821aa30f9d2daa007902a47778f862c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-25 21:22:06 -0400

    Handeling New accounts

--- routes/users/index.js
+++ routes/users/index.js
@@ -29,31 +29,36 @@ route.get("/profile/:username", (req, res) => {
     .then(async result => {
       const posts = result.data.graphql.user.edge_owner_to_timeline_media.edges;
       const postsLength = posts.length;
+      let aveLikes;
+      let aveComments;
+      let aveViews;
 
-      let totalLikes = await posts
-        .map(p => {
-          return p.node.edge_liked_by.count;
-        })
-        .reduce((a, b) => a + b);
+      if (postsLength) {
+        let totalLikes = await posts
+          .map(p => {
+            return p.node.edge_liked_by.count;
+          })
+          .reduce((a, b) => a + b);
 
-      let totalComments = await posts
-        .map(p => {
-          return p.node.edge_media_to_comment.count;
-        })
-        .reduce((a, b) => a + b);
+        let totalComments = await posts
+          .map(p => {
+            return p.node.edge_media_to_comment.count;
+          })
+          .reduce((a, b) => a + b);
 
-      let totalViews = await posts
-        .filter(p => p.node.is_video)
-        .map(p => {
-          return p.node.video_view_count;
-        })
-        .reduce((a, b) => a + b);
+        let totalViews = await posts
+          .filter(p => p.node.is_video)
+          .map(p => {
+            return p.node.video_view_count;
+          })
+          .reduce((a, b) => a + b);
 
-      let videoCount = await posts.filter(p => p.node.is_video).length;
+        let videoCount = await posts.filter(p => p.node.is_video).length;
 
-      const aveLikes = totalLikes / postsLength;
-      const aveComments = totalComments / postsLength;
-      const aveViews = totalViews / videoCount;
+        aveLikes = totalLikes / postsLength;
+        aveComments = totalComments / postsLength;
+        aveViews = totalViews / videoCount;
+      }
 
       const [addAccount] = await models
         .add("accounts", {
@@ -71,9 +76,11 @@ route.get("/profile/:username", (req, res) => {
             result.data.graphql.user.business_category_name,
           is_private: result.data.graphql.user.is_private,
           is_joined_recently: result.data.graphql.user.is_joined_recently,
-          average_likes: Math.round(aveLikes * 100) / 100,
-          average_comments: Math.round(aveComments * 100) / 100,
-          average_views: Math.round(aveViews * 100) / 100,
+          average_likes: postsLength ? Math.round(aveLikes * 100) / 100 : 0,
+          average_comments: postsLength
+            ? Math.round(aveComments * 100) / 100
+            : 0,
+          average_views: postsLength ? Math.round(aveViews * 100) / 100 : 0,
           posts_count:
             result.data.graphql.user.edge_owner_to_timeline_media.count
         })
@@ -81,32 +88,30 @@ route.get("/profile/:username", (req, res) => {
 
       const newAccount = await models.findBy("accounts", { id: addAccount });
 
-      await result.data.graphql.user.edge_owner_to_timeline_media.edges.map(
-        async post => {
-          const addPosts = await models.add("account_posts", {
-            caption: post.node.edge_media_to_caption.edges.length
-              ? post.node.edge_media_to_caption.edges[0].node.text
-              : "",
-            shortcode: post.node.shortcode,
-            comments_count: post.node.edge_media_to_comment.count,
-            likes_count: post.node.edge_liked_by.count,
-            view_count: post.node.edge_media_preview_like.count,
-            comments_disabled: post.node.comments_disabled,
-            taken_at_timestamp: post.node.taken_at_timestamp,
-            is_video: post.node.is_video,
-            accessibility_caption: post.node.accessibility_caption,
-            account_id: addAccount,
-            engagment:
-              Math.round(
-                ((post.node.edge_liked_by.count +
-                  post.node.edge_media_to_comment.count) /
-                  newAccount.follower_count) *
-                  100 *
-                  100
-              ) / 100
-          });
-        }
-      );
+      await posts.map(async post => {
+        const addPosts = await models.add("account_posts", {
+          caption: post.node.edge_media_to_caption.edges.length
+            ? post.node.edge_media_to_caption.edges[0].node.text
+            : "",
+          shortcode: post.node.shortcode,
+          comments_count: post.node.edge_media_to_comment.count,
+          likes_count: post.node.edge_liked_by.count,
+          view_count: post.node.edge_media_preview_like.count,
+          comments_disabled: post.node.comments_disabled,
+          taken_at_timestamp: post.node.taken_at_timestamp,
+          is_video: post.node.is_video,
+          accessibility_caption: post.node.accessibility_caption,
+          account_id: addAccount,
+          engagment:
+            Math.round(
+              ((post.node.edge_liked_by.count +
+                post.node.edge_media_to_comment.count) /
+                newAccount.follower_count) *
+                100 *
+                100
+            ) / 100
+        });
+      });
 
       const account_posts = await models.findAllBy("account_posts", {
         account_id: addAccount
