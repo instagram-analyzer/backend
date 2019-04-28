9431b43055abb8001b2b010d4c882d84ba7cb7ca
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-28 19:15:55 -0400

    Posts fetching on cron jobs

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,21 +1,10 @@
 const route = require("express").Router();
 const axios = require("axios");
+const cron = require("node-cron");
 const models = require("../../common/helpers.js");
 const { cookieSet } = require("./cookie");
 const BASE_URL = "https://www.instagram.com/graphql/query/?";
-
 // const getFollowers = require("./getFollowers");
-
-axios.defaults.withCredentials = true;
-
-// const proxy = {
-//   host: "190.202.24.66",
-//   port: 3128
-//   // auth: {
-//   //   username: 'some_login',
-//   //   password: 'some_pass'
-//   // }
-// };
 let cookieString = "";
 let currentCookie = 0;
 
@@ -34,8 +23,8 @@ const getCookie = () => {
 
 getCookie();
 
-route.get("/profile/:username", (req, res) => {
-  const { username } = req.params;
+function fetchUser(username) {
+  let newAccount;
 
   axios
     .get(`https://www.instagram.com/${username}/?__a=1`, {
@@ -112,7 +101,7 @@ route.get("/profile/:username", (req, res) => {
         })
         .returning("id");
 
-      const newAccount = await models.findBy("accounts", { id: addAccount });
+      newAccount = await models.findBy("accounts", { id: addAccount });
 
       await posts.map(async post => {
         const addPosts = await models.add("account_posts", {
@@ -144,11 +133,143 @@ route.get("/profile/:username", (req, res) => {
       });
 
       newAccount.posts = account_posts;
+    });
 
-      res.json(newAccount);
+  return newAccount;
+}
+
+const startCronJob = username => {
+  cron.schedule("0-59/1 * * * * *", () => {
+    console.log("Fetching posts");
+    fetchUser(username);
+  });
+};
+
+axios.defaults.withCredentials = true;
+
+// const proxy = {
+//   host: "190.202.24.66",
+//   port: 3128
+//   // auth: {
+//   //   username: 'some_login',
+//   //   password: 'some_pass'
+//   // }
+// };
+
+let startCron = true;
+
+route.get("/profile/:username", async (req, res) => {
+  const { username } = req.params;
+  axios
+    .get(`https://www.instagram.com/${username}/?__a=1`, {
+      headers: {
+        Cookie: cookieString
+      }
     })
-    .catch(() => {
-      res.json({ message: "Broken" });
+    .then(async result => {
+      const posts = result.data.graphql.user.edge_owner_to_timeline_media.edges;
+      const postsLength = posts.length;
+      let aveLikes;
+      let aveComments;
+      let aveViews;
+      let totalEngagment;
+
+      if (postsLength) {
+        let totalLikes = await posts
+          .map(p => {
+            return p.node.edge_liked_by.count;
+          })
+          .reduce((a, b) => a + b);
+
+        let totalComments = await posts
+          .map(p => {
+            return p.node.edge_media_to_comment.count;
+          })
+          .reduce((a, b) => a + b);
+
+        // let totalViews = await posts
+        //   .filter(p => p.node.is_video)
+        //   .map(p => {
+        //     return p.node.video_view_count;
+        //   })
+        //   .reduce((a, b) => a + b);
+
+        // let videoCount = await posts.filter(p => p.node.is_video).length;
+
+        totalEngagment =
+          Math.round(
+            ((totalLikes + totalComments) /
+              result.data.graphql.user.edge_followed_by.count) *
+              100
+          ) / 100;
+        aveLikes = totalLikes / postsLength;
+        aveComments = totalComments / postsLength;
+        // aveViews = totalViews / videoCount;
+      }
+
+      const [addAccount] = await models
+        .add("accounts", {
+          instagram_id: result.data.graphql.user.id,
+          account_bio: result.data.graphql.user.biography,
+          account_bio_url: result.data.graphql.user.external_url,
+          account_username: result.data.graphql.user.username,
+          account_image_url: result.data.graphql.user.profile_pic_url_hd,
+          follower_count: result.data.graphql.user.edge_followed_by.count,
+          following_count: result.data.graphql.user.edge_follow.count,
+          hightlight_reel_count: result.data.graphql.user.highlight_reel_count,
+          full_name: result.data.graphql.user.full_name,
+          is_verified: result.data.graphql.user.is_verified,
+          is_business_account: result.data.graphql.user.is_business_account,
+          business_category_name:
+            result.data.graphql.user.business_category_name,
+          is_private: result.data.graphql.user.is_private,
+          is_joined_recently: result.data.graphql.user.is_joined_recently,
+          average_likes: postsLength ? Math.round(aveLikes * 100) / 100 : 0,
+          average_comments: postsLength
+            ? Math.round(aveComments * 100) / 100
+            : 0,
+          // average_views: postsLength ? Math.round(aveViews * 100) / 100 : 0,
+          total_engagement: postsLength ? totalEngagment : 0,
+          posts_count:
+            result.data.graphql.user.edge_owner_to_timeline_media.count
+        })
+        .returning("id");
+
+      newAccount = await models.findBy("accounts", { id: addAccount });
+
+      await posts.map(async post => {
+        const addPosts = await models.add("account_posts", {
+          caption: post.node.edge_media_to_caption.edges.length
+            ? post.node.edge_media_to_caption.edges[0].node.text
+            : "",
+          shortcode: post.node.shortcode,
+          comments_count: post.node.edge_media_to_comment.count,
+          likes_count: post.node.edge_liked_by.count,
+          view_count: post.node.edge_media_preview_like.count,
+          comments_disabled: post.node.comments_disabled,
+          taken_at_timestamp: post.node.taken_at_timestamp,
+          is_video: post.node.is_video,
+          accessibility_caption: post.node.accessibility_caption,
+          account_id: addAccount,
+          engagment:
+            Math.round(
+              ((post.node.edge_liked_by.count +
+                post.node.edge_media_to_comment.count) /
+                newAccount.follower_count) *
+                100 *
+                100
+            ) / 100
+        });
+      });
+
+      const account_posts = await models.findAllBy("account_posts", {
+        account_id: addAccount
+      });
+
+      newAccount.posts = account_posts;
+      startCron && startCronJob(username);
+      startCron = false;
+      res.json(newAccount);
     });
 });
 
