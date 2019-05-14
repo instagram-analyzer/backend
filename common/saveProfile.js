9d95bdc0cf472647156d961e7832b30674639f10
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-05-13 23:32:38 -0400

    Changed timezone

--- common/saveProfile.js
+++ common/saveProfile.js
@@ -1,40 +1,33 @@
 const models = require("../common/helpers");
 const db = require("../data/dbConfig");
-const moment = require("moment-timezone").tz("Etc/GMT");
+const moment = require("moment-timezone");
 
 const saveProfile = async profile => {
-  console.log(`saving ${profile.username}'s profile`);
-  //   const savedProfile = await models
-  //     .findAllBy("updating_accounts", { account_username: profile.username })
-  //     .where("created_at", "now() - interval '1 year'")
-  //     .orderBy("created_at", "desc");
-
-  // 2019-05-12T00:00:00Z  - 2019-05-12T11:59:00Z
-
   const savedProfile = await db.raw(
     `SELECT * FROM updating_accounts WHERE account_username = '${
       profile.username
-    }' AND created_at >= '${moment.format(
-      "YYYY-MM-DD"
-    )}T00:00:00Z' AND created_at < '${moment.format(
-      "YYYY-MM-DD"
-    )}T23:59:00Z' ORDER BY created_at desc `
+    }' AND created_at >= '${moment()
+      .tz("Etc/GMT")
+      .format("YYYY-MM-DD")}T00:00:00Z' AND created_at < '${moment()
+      .tz("Etc/GMT")
+      .format("YYYY-MM-DD")}T23:59:00Z' `
   );
 
   const yesterdayProfile = await db.raw(
     `SELECT * FROM updating_accounts WHERE account_username = '${
       profile.username
-    }' AND created_at >= '${moment
+    }' AND created_at >= '${moment()
+      .tz("Etc/GMT")
       .subtract(1, "days")
-      .format("YYYY-MM-DD")}T00:00:00Z' AND created_at < '${moment
+      .format("YYYY-MM-DD")}T00:00:00Z' AND created_at < '${moment()
+      .tz("Etc/GMT")
       .subtract(1, "days")
       .format("YYYY-MM-DD")}T23:59:00Z' ORDER BY created_at desc `
   );
 
-  console.log(savedProfile.rows, yesterdayProfile.rows);
+  if (savedProfile.rows[0]) {
+    console.log(`updating ${profile.username}'s profile`);
 
-  if (await savedProfile.rows[0]) {
-    console.log("I AM UPDATING");
     await models.update("updating_accounts", savedProfile.rows[0].id, {
       account_bio: profile.biography,
       account_bio_url: profile.external_url,
@@ -57,7 +50,8 @@ const saveProfile = async profile => {
         : 0
     });
   } else {
-    console.log("I AM ADDING");
+    console.log(`saving ${profile.username}'s profile`);
+
     await models.add("updating_accounts", {
       account_bio: profile.biography,
       account_bio_url: profile.external_url,
